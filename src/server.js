9453b8607cfe67c94fc22c1560da3ff1abdccb82
basant006neo-cmyk <basant006.neo@gmail.com>
2026-02-05 17:19:04 +0530

    fix: updated for deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
+import * as dotenv from 'dotenv'
+
 import app from './app.js';
+
+dotenv.config()
+
 // import config from './config/index.js';
 
+const PORT= process.env.PORT || 3000;
+
 app.listen(config.port, () => {
-  console.log(`🚀 Server running on port ${config.port}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
