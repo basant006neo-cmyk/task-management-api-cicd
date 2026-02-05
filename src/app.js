1476e11110484559ded6bf44804a5bf446e5b3c3
basant006neo-cmyk <basant006.neo@gmail.com>
2026-02-05 15:36:50 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+// import routes from './routes/index.js';
+// import errorMiddleware from './middlewares/error.middleware.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+// app.use('/api', routes);
+// app.use(errorMiddleware);
+
+export default app;
